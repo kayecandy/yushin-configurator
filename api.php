a3974eb428add0e715a82afde12106323619f56d
Candice Canoso <mail@cndce.me>
2021-04-14 06:48:08 +0800

    Moved test.php to api.php

--- api.php
+++ api.php
@@ -1,47 +1,43 @@
-<?php
+<?php 
+	require_once( 'config.php' );
 
-	// require_once( 'config.php' );
+	function get_quote_num(){
+		try{
+			$conn = get_connection();
 
+			$stmt = $conn->prepare('INSERT INTO quotes (quotenum, firstname, lastname, phone, email) VALUES (:quotenum, :firstname, :lastname, :phone, :email)');
 
-	// function get_quote_num(){
-	// 	try{
-	// 		$conn = get_connection();
+			$stmt->bindParam(':quotenum', $_POST['format_quote_num']);
+			$stmt->bindParam(':firstname', $_POST['first_name']);
+			$stmt->bindParam(':lastname', $_POST['last_name']);
+			$stmt->bindParam(':phone', $_POST['phone']);
+			$stmt->bindParam(':email', $_POST['email']);
 
-	// 		$stmt = $conn->prepare('INSERT INTO quotes (quotenum, firstname, lastname, phone, email) VALUES (:quotenum, :firstname, :lastname, :phone, :email)');
+			$stmt->execute();
 
-	// 		$stmt->bindParam(':quotenum', $_POST['format_quote_num']);
-	// 		$stmt->bindParam(':firstname', $_POST['first_name']);
-	// 		$stmt->bindParam(':lastname', $_POST['last_name']);
-	// 		$stmt->bindParam(':phone', $_POST['phone']);
-	// 		$stmt->bindParam(':email', $_POST['email']);
+			return $_POST['format_quote_num'] . $conn->lastInsertId();
+		}catch(PDOException $e){
+			echo $e->getMessage();
 
-	// 		$stmt->execute();
-
-	// 		return $conn->lastInsertId();
-	// 	}catch(PDOException $e){
-	// 		echo $e->getMessage();
-
-	// 		return '';
-	// 	}
+			return '';
+		}
 		
-	// }
+	}
 
 
-	// function get_connection(){
-	// 	try{
-	// 		$conn = new PDO('mysql:host=' . DB_SERVER . ':' . DB_PORT . ';dbname=' . DB_SCHEMA, DB_USERNAME, DB_PASSWORD);
+	function get_connection(){
+		try{
+			$conn = new PDO('mysql:host=' . DB_SERVER . ':' . DB_PORT . ';dbname=' . DB_SCHEMA, DB_USERNAME, DB_PASSWORD);
 
-	// 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	// 		return $conn;
-	// 	}catch(PDOException $e){
-	// 		echo $e->getMessage();
-	// 	}
+			return $conn;
+		}catch(PDOException $e){
+			echo $e->getMessage();
+		}
 		
-	// }
+	}
 
-	// echo get_quote_num();
-
+	echo get_quote_num();
+	
 ?>
-
-hello world
